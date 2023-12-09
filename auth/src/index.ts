e71fafe208a9dccea890c5384b7020f6723d8dc1
Htet <htet@3sam.io>
2023-12-09 20:09:24 +0700

    feat: add ingress service infra

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const port = 6000
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentUser', (req, res) => {
+    res.send('Hi there');
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
 }) 
