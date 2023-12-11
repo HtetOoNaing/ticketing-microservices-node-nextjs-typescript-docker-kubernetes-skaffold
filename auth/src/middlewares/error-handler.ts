789c96459a01328ba24e2901e9d27326ea2fc33f
Htet <htet@3sam.io>
2023-12-11 23:48:10 +0700

    feat: use CustomError in error-handler middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,15 +1,9 @@
 import { NextFunction, Request, Response } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
-    if (err instanceof RequestValidationError) {
-        console.log(' request validation error ')
-        return res.status(err.statusCode).json({ errors: err.serializeErrors() })
-    }
-    if (err instanceof DatabaseConnectionError) {
-        console.log(' db connection error ')
+    if (err instanceof CustomError) {
         return res.status(err.statusCode).json({ errors: err.serializeErrors() })
     }
 
