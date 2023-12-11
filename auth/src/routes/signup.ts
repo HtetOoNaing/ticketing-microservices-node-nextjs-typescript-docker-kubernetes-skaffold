c6abbff9e0210595164a827674e6c09f69798b87
Htet <htet@3sam.io>
2023-12-11 09:49:46 +0700

    feat: handling validation errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -7,7 +7,14 @@ router.post('/api/users/signup',[
     body('email').isEmail().withMessage('Email must be valid'), 
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
 ], (req: Request, res: Response) => {
-    res.send('Hi there');
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).send(errors.array())
+    }
+    
+    const { email, password } = req.body;
+    console.log('Creating a new user...');
+    res.send({})
 })
 
 export { router as signupRouter };  
