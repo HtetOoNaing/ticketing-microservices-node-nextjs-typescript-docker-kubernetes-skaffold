2bbd543b528f8450a8e3357858ef9b0cf0264bcf
Htet <htet@3sam.io>
2023-12-12 22:20:01 +0700

    feat: add validateRequest middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,17 +1,13 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
 router.post('/api/users/signin', [
     body('email').isEmail().withMessage('Email must be valid'), 
     body('password').trim().notEmpty().withMessage('Password must be filled')
-], (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+], validateRequest, (req: Request, res: Response) => {
     const { email, password } = req.body;
 })
 
