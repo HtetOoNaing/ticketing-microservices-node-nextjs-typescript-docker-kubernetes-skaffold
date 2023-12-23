1f67f383fa684496f90efcc5ba5bd51656952f8d
Htet <htet@3sam.io>
2023-12-23 12:18:58 +0700

    feat: update signup page and input component

--- client/src/app/auth/signup/page.js
+++ client/src/app/auth/signup/page.js
@@ -6,10 +6,10 @@ export default function Signup() {
     <form className="p-2">
       <h1 className="text-blue-500 text-xl text-center">Signup Page</h1>
       <div>
-        <Input type="email" id="email" placeholder="example@company.com" containerClassName="mt-4" />
+        <Input type="email" id="email" placeholder="example@company.com" label="Email address" containerClassName="mt-4" />
+        <Input type="password" id="password" placeholder="password" label="Password" containerClassName="mt-4" />
         <Button className="mt-4">Signup</Button>
       </div>
     </form>
   )
 }
-  
