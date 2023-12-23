526fee45a5067dd0a15beefe846e9f9f9b684396
Htet <htet@3sam.io>
2023-12-24 00:01:23 +0700

    feat: add signup form and update input

--- client/src/app/auth/signup/page.js
+++ client/src/app/auth/signup/page.js
@@ -1,14 +1,31 @@
+"use client";
+
 import Button from "@/app/components/button";
 import Input from "@/app/components/input";
-
+import { useState } from "react";
 export default function Signup() {
+  const [userinfo, setUserinfo] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setUserinfo(info => ({...info, [name]: value}));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('userinfo', userinfo)
+  }
+
   return (
-    <form className="p-2">
+    <form onSubmit={handleSubmit} className="p-2">
       <h1 className="text-blue-500 text-xl text-center">Signup Page</h1>
       <div>
-        <Input type="email" id="email" placeholder="example@company.com" label="Email address" containerClassName="mt-4" />
-        <Input type="password" id="password" placeholder="password" label="Password" containerClassName="mt-4" />
-        <Button className="mt-4">Signup</Button>
+        <Input type="email" name="email" value={userinfo.email} onChange={handleOnChange} placeholder="example@company.com" label="Email address" containerClassName="mt-4" />
+        <Input type="password" name="password" value={userinfo.password} onChange={handleOnChange} placeholder="password" label="Password" containerClassName="mt-4" />
+        <Button type="submit" className="mt-4">Signup</Button>
       </div>
     </form>
   )
--- client/src/app/components/input.js
+++ client/src/app/components/input.js
@@ -1,8 +1,8 @@
-export default function Input({type, id, placeholder, value, onChange, label, className = '', containerClassName = '', labelClassName = '' }) {
+export default function Input({type, name, placeholder, value, onChange, label, className = '', containerClassName = '', labelClassName = '' }) {
     return (
         <div className={`${containerClassName}`}>
-            <label htmlFor={id} className={`block ml-1 mb-2 text-sm font-medium text-gray-700 ${labelClassName}`}>{label}</label>
-            <input type={type} id={id} value={value} onChange={onChange} className={`bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus:outline-none block w-full px-2.5 py-2 ${className}`} placeholder={placeholder} />
+            <label htmlFor={name} className={`block ml-1 mb-2 text-sm font-medium text-gray-700 ${labelClassName}`}>{label}</label>
+            <input type={type} id={name} name={name} value={value} onChange={onChange} className={`bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus:outline-none block w-full px-2.5 py-2 ${className}`} placeholder={placeholder} />
         </div>
     )
 }
